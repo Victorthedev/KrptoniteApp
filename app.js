796762b6d75e076888fccf6d2dca98b9a0227fad
Victor Ubahakwe <ubahakweemeka@gmail.com>
2024-06-04 13:41:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use('/auth', authRoutes);
 app.use('/files', fileRoutes);
 
-mongoose.connect('mongodb+srv://node-auth:node@cluster0.mn7emsg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
